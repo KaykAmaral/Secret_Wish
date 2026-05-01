7c4561fd672401392eae5ab7d5bd147b79c1424a
KaykAmaral <kayaquiurbano@gmail.com>
2026-05-01 01:18:25 -0300

    Initial Spring project

---
+++ backend/src/main/java/com/example/springtest/TestController.java
@@ -0,0 +1,12 @@
+package com.example.springtest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Spring Boot está rodando com sucesso!";
+    }
+}
