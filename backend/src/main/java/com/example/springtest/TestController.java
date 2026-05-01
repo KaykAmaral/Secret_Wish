a593fdb9dca38e6405bdd55204fbad3ea58d1685
KaykAmaral <kayaquiurbano@gmail.com>
2026-05-01 13:35:33 -0300

    Frontend Test

--- backend/src/main/java/com/example/springtest/TestController.java
+++ backend/src/main/java/com/example/springtest/TestController.java
@@ -1,12 +1,16 @@
 package com.example.springtest;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class TestController {
-    @GetMapping("/hello")
-    public String hello() {
-        return "Spring Boot está rodando com sucesso!";
+    @GetMapping("/test")
+    public String test() {
+        return "Conexão com o Backend estabelecida com sucesso!";
     }
 }
